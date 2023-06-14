ed56b791cf07997b8f316484838594af7229b7ac
Nix Matrix <nbarbera@feldinc.com>
2023-06-14 11:02:35 -0400

    Added Unitree and Dog Buddy!

---
+++ dog.py
@@ -0,0 +1,61 @@
+import sys
+import time
+
+sys.path.append('unitree_legged_sdk/lib/python/arm64')
+import robot_interface as sdk
+
+## comm.h
+
+HIGHLEVEL = 0xee
+
+rate = 500       # 500hz
+dt = 1.0 / rate  # time between commands
+
+def main():
+    udp = sdk.UDP(HIGHLEVEL, 8080, "192.168.123.220", 8082)
+
+    cmd = sdk.HighCmd()
+    state = sdk.HighState()
+    udp.InitCmdData(cmd)
+
+    cmd.mode = 0
+    cmd.gaitType = 0
+    cmd.speedLevel = 0
+    cmd.footRaiseHeight = 0
+    cmd.bodyHeight = 0
+    cmd.euler = [0, 0, 0]
+    cmd.velocity = [0, 0]
+    cmd.yawSpeed = 0.0
+    cmd.reserve = 0
+#    for i in range(2000)
+#        udp.Recv()
+#        udp.GetRecv(state)
+#        print(state.motorState[0].temperature)
+#        udp.SetSend(cmd)
+#        udp.Send()
+#        time.sleep(dt)
+
+    for i in range(int(4/dt)):
+        cmd.mode = 6 # stand
+        udp.SetSend(cmd)
+        udp.Send()
+        time.sleep(dt)
+
+
+    for i in range(int(4/dt)):
+        cmd.mode = 5 # sit
+        udp.SetSend(cmd)
+        udp.Send()
+        time.sleep(dt)
+
+
+    #for i in range(int(2/dt)):
+    #    cmd.mode = 1 #  Active stand
+    #    cmd.euler = [0.0, 0.3, 0.0] # Down
+    #
+
+    #for i in range(int(1/dt)):
+    #    cmd.mode = 1 # Active stand
+    #    udp.SetSend(cmd)
+    #    
+main()
